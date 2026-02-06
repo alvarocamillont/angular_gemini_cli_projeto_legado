d27d3626423925d6ec9b36dc8fc5701b657059bf
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2026-02-06 15:57:26 -0300

    add routing configuration and main template for DigimonList component

--- projeto-legado/src/app/app-routing-module.ts
+++ projeto-legado/src/app/app-routing-module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DigimonList } from './digimon-list/digimon-list';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'digimons',
+    component: DigimonList
+  },
+  {
+    path: '',
+    redirectTo: '/digimons',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
